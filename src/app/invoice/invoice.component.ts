419ac100861836186d4b76f3ff4dd3568a229f5a
abmrifat1 <rifatabm1@gmail.com>
2020-02-13 18:41:36 +0600

    onclick event

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,31 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-invoice',
-  templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.scss']
+  selector: "app-invoice",
+  templateUrl: "./invoice.component.html",
+  styleUrls: ["./invoice.component.scss"]
 })
 export class InvoiceComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public sms="";
+  public hasError = true;
+  public successText = "text_success";
+  public specialText = true;
+  public messageClass = {
+    text_success: !this.hasError,
+    text_danger: this.hasError,
+    text_special: this.specialText
+  };
 
-  public hasError=true;
-  public successText='text_success';
-  public specialText= true;
-  public messageClass= {
-    "text_success":!this.hasError,
-    "text_danger":this.hasError,
-     "text_special":this.specialText,
+  public multyStyle = {
+    color: "yellow",
+    fontStyle: "italic"
+  };
+  ngOnInit(): void {}
+  getsms(event) {
+    this.sms="hi btn click"
+    console.log("hi here!!!!");
+    console.log("all event",event);
   }
-
-  public multyStyle ={
-    color:'yellow',
-    fontStyle: 'italic',
-    
-  }
-  ngOnInit(): void {
-
-
-  }
-
 }
