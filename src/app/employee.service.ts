3cf1d3ed3b0334a60358c889b384775651836f9c
abmrifat1 <rifatabm1@gmail.com>
2020-02-18 13:35:09 +0600

    http error handler

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,29 +1,30 @@
-import { IEmployee } from './employee';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { IEmployee } from "./employee";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import "rxjs/add/operator/catch";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class EmployeeService {
-
-  private _url:string= "/assets/data/employees.json"
+  private _url: string = "/assets/data/employees.json";
 
   constructor(private http: HttpClient) {}
- 
-//  http request and model
-  getEmployess(){
-    return this.http.get<IEmployee[]>(this._url);
+
+  //  http request and model
+  getEmployess(): Observable<IEmployee[]> {
+    return this.http.get<IEmployee[]>(this._url).catch(this.errorHandler);
   }
 
+  errorHandler(error: HttpErrorResponse) {}
+
   // service
-    // getEmployess(){
-    //   return [
-    //     {'id':5998,'name':'abm'},
-    //     {'id':6001,'name':'ars'},
-    //     {'id':4444,'name':'btn'}
-    //   ]
-    // }
-   
+  // getEmployess(){
+  //   return [
+  //     {'id':5998,'name':'abm'},
+  //     {'id':6001,'name':'ars'},
+  //     {'id':4444,'name':'btn'}
+  //   ]
+  // }
 }
- 
